8547e937591e04e58c1a7526e806c39b2afc0e5b
miko <mikomai@can-dao.com>
2020-07-27 18:15:10 +0800

    success run

--- paopao/src/main/java/com/miko/paopao/controller/BaseController.java
+++ paopao/src/main/java/com/miko/paopao/controller/BaseController.java
@@ -2,13 +2,15 @@ package com.miko.paopao.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/base")
+@ResponseBody
 public class BaseController {
 
     @RequestMapping("/getBase")
     public String getBase(){
-        return "base";
+        return "base222";
     }
 }
