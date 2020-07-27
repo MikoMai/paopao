33a0df8f44c5416bfdb37eb176b21b99d8b0b394
miko <mikomai@can-dao.com>
2020-07-27 16:21:31 +0800

    first commit

---
+++ paopao/src/main/java/com/miko/paopao/PaopaoApplication.java
@@ -0,0 +1,13 @@
+package com.miko.paopao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaopaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaopaoApplication.class, args);
+    }
+
+}
---
+++ paopao/src/test/java/com/miko/paopao/PaopaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miko.paopao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaopaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
