1a698e446703c17e90aec69bf84b6f05fedcc678
miko <mikomai@can-dao.com>
2020-07-30 18:04:37 +0800

    user基本操作

--- paopao/src/main/java/com/miko/paopao/controller/UserController.java
+++ paopao/src/main/java/com/miko/paopao/controller/UserController.java
@@ -17,28 +17,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.lang.reflect.Method;
 
 @Controller
-@RequestMapping("/base")
+@RequestMapping("/user")
 @ResponseBody
-public class BaseController {
+public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/getBase")
-    public RetResult<Object> getBase(@RequestBody User user){
+    @RequestMapping("/saveUser")
+    public RetResult<Object> saveUser(@RequestBody User user){
         userService.saveUser(user);
         return RetResponse.makeOKRsp();
     }
 
 
     @RequestMapping(value = "/getUser")
-    public RetResult<User> getBase(@RequestParam(value = "userId") Long userId){
+    public RetResult<User> getUser(@RequestParam(value = "userId") Long userId){
         User user=userService.getUserById(userId);
         return RetResponse.makeOKRsp(user);
     }
 
     @RequestMapping(value = "/getUserPage")
-    public RetResult<Page<User>> getBase(@RequestParam(value = "pageSize") Integer pageSize,@RequestParam(value = "page") Integer page){
+    public RetResult<Page<User>> getUserPage(@RequestParam(value = "pageSize") Integer pageSize,@RequestParam(value = "page") Integer page){
         Pageable pageable =  PageRequest.of(page-1,pageSize);
         Page<User> userPage=userService.getUserPage(pageable);
         return RetResponse.makeOKRsp(userPage);
